88428fa2d24f6d98c8b1d0e40b4a5032cc25c127
BoboMac <alexbobomac@gmail.com>
2023-11-06 14:55:17 +0200

    yolooo

--- GameJam/main.cpp
+++ GameJam/main.cpp
@@ -5,12 +5,15 @@
 #include "util.h"
 #include "game.h"
 #include "render.h"
+extern "C" {
+#include "vendor/noise1234.h"
+}
 
 int main() {
 	char map[MAP_W+2][MAP_H+2];
 	for (int i = 1; i < MAP_W + 1; i++)
 		for (int j = 1; j < MAP_H + 1; j++)
-			map[i][j] = abs(map[i][j] % 4);
+			map[i][j] = (noise2(i/10.0f, j / 10.0f) > -.25f ? DIRT : EMPTY);
 	
 
 	sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
