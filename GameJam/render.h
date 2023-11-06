f8c6e17f3d2d9968abd8517e7f50c0d6f98f92a0
Alex Obreja <alex.obreja2005@gmail.com>
2023-11-06 13:49:30 +0200

    rdgdfgdzgd

--- GameJam/render.h
+++ GameJam/render.h
@@ -1,7 +1,8 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+#include "util.h"
 
-void DrawMap(char** map) {
+void DrawMap(char** map, sf::RenderWindow& window) {
 
 	/*
 	sf::Texture dirt_tex[4], water_tex, fire_tex;
@@ -12,13 +13,22 @@ void DrawMap(char** map) {
 	water_tex.loadFromFile("res/");
 	fire_tex.loadFromFile("res/");
 	*/
-	
+
 	sf::RectangleShape tile({ TILE_SIZE, TILE_SIZE });
+	sf::Color colors[5] = { sf::Color::Black, sf::Color::Green, sf::Color::Blue, {139, 69, 19} };
 
 	for (int i = 0; i < MAP_W; i++) {
 		for (int j = 0; j < MAP_H; j++) {
-			sf::
+			if (map[i][j] != DIRT)
+				tile.setFillColor(colors[map[i][j]]);
+			else {
+				if ((i - 1 >= 0 && map[i - 1][j] != DIRT) || (i + 1 < 0 <= MAP_W && map[i - 1][j] != DIRT) || (j - 1 >= 0 && map[i][j - 1] != DIRT) || (j + 1 < 0 <= MAP_H && map[i][j + 1] != DIRT))
+					tile.setFillColor(colors[4]);
+				else
+					tile.setFillColor(colors[map[i][j]]);
+			}
+			tile.setPosition({ i * TILE_SIZE, j * TILE_SIZE });
+			window.draw(tile);
 		}
 	}
-	
 }
