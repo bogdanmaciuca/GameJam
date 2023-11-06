c6c9412bc40f8dc33a1f47874445791a63b745b4
BoboMac <alexbobomac@gmail.com>
2023-11-06 15:44:12 +0200

    final

--- GameJam/game.h
+++ GameJam/game.h
@@ -1,6 +1,9 @@
 #pragma once
 #include "util.h"
 #include <SFML/Window.hpp>
+extern "C" {
+#include "vendor/noise1234.h"
+}
 
 char _oldMap[MAP_W + 2][MAP_H + 2];
 void UpdateMap(char map[MAP_W + 2][MAP_H + 2]) {
@@ -18,4 +21,24 @@ void UpdateMap(char map[MAP_W + 2][MAP_H + 2]) {
 			}
 		}
 	}
+}
+
+void CreateMap(char map[MAP_W + 2][MAP_H + 2]) {
+	for (int i = 0; i <= MAP_W; i++)
+		for (int j = 0; j <= MAP_H; j++)
+			map[i][j] = -1;
+
+	for (int i = 1; i < MAP_W + 1; i++)
+		for (int j = 1; j < MAP_H + 1; j++)
+			map[i][j] = (noise2(i / 10.0f, j / 10.0f) > -.25f ? DIRT : EMPTY);
+
+	// Generate water
+	int waterStartX = 0, waterStartY = 0;
+	srand(time(0));
+	do {
+		waterStartX = rand() % MAP_W + 1;
+		waterStartY = rand() % MAP_H + 1;
+	} while (map[waterStartX][waterStartY] != EMPTY);
+	map[waterStartX][waterStartY] = WATER;
+
 }
--- GameJam/main.cpp
+++ GameJam/main.cpp
@@ -7,32 +7,13 @@
 #include "render.h"
 #include <stdlib.h>
 #include <iostream>
-extern "C" {
-#include "vendor/noise1234.h"
-}
 #include <iostream>
 
 int main() {
 	char map[MAP_W+2][MAP_H+2];
-	for (int i = 0; i <= MAP_W; i++)
-		for (int j = 0; j <= MAP_H; j++)
-			map[i][j] = -1;
+	CreateMap(map);
 
-	for (int i = 1; i < MAP_W+1; i++)
-		for (int j = 1; j < MAP_H+1; j++)
-			map[i][j] = (noise2(i/10.0f, j / 10.0f) > -.25f ? DIRT : EMPTY);
-
-	// Generate water
-	int waterStartX = 0, waterStartY = 0;
-
-	srand(time(0));
-	do {
-		waterStartX = rand() % MAP_W + 1;
-		waterStartY = rand() % MAP_H + 1;
-	} while (map[waterStartX][waterStartY] != EMPTY);
-	map[waterStartX][waterStartY] = WATER;
-	
-	sf::RenderWindow window(sf::VideoMode(WND_WIDTH, WND_HEIGHT), "My window");
+	sf::RenderWindow window(sf::VideoMode(WND_WIDTH, WND_HEIGHT), "Minesweeper");
 	
 	window.setFramerateLimit(FPS_LIMIT);
 	unsigned long long ticks = 0;
