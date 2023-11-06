a6714f7c326c9a01045487db9606d067ed3ece8e
Alex Obreja <alex.obreja2005@gmail.com>
2023-11-06 13:11:52 +0200

    fgfg

--- GameJam/main.cpp
+++ GameJam/main.cpp
@@ -9,29 +9,6 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 
-const int WND_WIDTH = 1024;
-const int WND_HEIGHT = 768;
-const int TILE_SIZE = 16;
-const int MAP_W = WND_WIDTH / TILE_SIZE;
-const int MAP_H = WND_HEIGHT / TILE_SIZE;
-
-enum {
-	EMPTY,
-	DIRT,
-	WATER,
-	FIRE
-};
-
-void DrawMap(char **map) {
-	sf::Texture dirt_tex, water_tex, fire_tex;
-	texture.loadFromFile("res/")
-	for (int i = 0; i < MAP_W; i++) {
-		for (int j = 0; j < MAP_H; j++) {
-			sf::
-		}
-	}
-}
-
 int main() {
 	char map[TILE_NUM_X][TILE_NUM_Y];
 	sf::Window window(sf::VideoMode(800, 600), "My window");
--- GameJam/render.h
+++ GameJam/render.h
@@ -1,1 +1,24 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+
+void DrawMap(char** map) {
+
+	/*
+	sf::Texture dirt_tex[4], water_tex, fire_tex;
+	dirt_tex[0].loadFromFile("res/");
+	dirt_tex[1].loadFromFile("res/");
+	dirt_tex[2].loadFromFile("res/");
+	dirt_tex[4].loadFromFile("res/");
+	water_tex.loadFromFile("res/");
+	fire_tex.loadFromFile("res/");
+	*/
+	
+	sf::RectangleShape tile({ TILE_SIZE, TILE_SIZE });
+
+	for (int i = 0; i < MAP_W; i++) {
+		for (int j = 0; j < MAP_H; j++) {
+			sf::
+		}
+	}
+	
+}
